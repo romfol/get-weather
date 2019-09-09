a30dbe022ae3bd956ac298c11489a136b4327877
romfol <romanfoltyn1@gmail.com>
2019-09-09 21:14:38 +0300

    fixes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const address = process.argv[2]
 if (!address) {
   console.log("Provide address!");
 } else {
-  geocode('cherkasy', (error, data) => {
+  geocode(address, (error, data) => {
   if (error) {
     return console.log(error);
   }
