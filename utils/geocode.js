2b7052b511d77b79a9724ce4812300853acf4847
romfol <romanfoltyn1@gmail.com>
2019-09-10 20:58:21 +0300

    refactored

--- utils/geocode.js
+++ utils/geocode.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoicm9tZm9sIiwiYSI6ImNrMDAwcmhvMDBidWgzaHF0d21qZTZpN2wifQ.tQkCXh2owYHqE1FFXwSBxg&limit=1`;
-  request({ url, json: true }, (err, { body = null}) => {
+  request({ url, json: true }, (err, { body }) => {
     
     if (err) {
       callback('unable to connect');
